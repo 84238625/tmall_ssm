452137c7bd7fcd1e030cb33278a1a402111b28fd
84238625 <84238625@qq.com>
2019-09-19 17:31:21 +0800

    分类更新update

--- src/main/java/com/how2java/tmall/mapper/CategoryMapper.java
+++ src/main/java/com/how2java/tmall/mapper/CategoryMapper.java
@@ -5,8 +5,8 @@ import com.how2java.tmall.pojo.Category;
 import java.util.List;
  
 public interface CategoryMapper {
-     List<Category> list(Page page);  //分页查询方法
-     int total(); //查询总数方法
+     List<Category> list();  //分页查询方法
+    /* int total(); //查询总数方法*/
      void add(Category category);//增加分类
      void delete(int id);//删除分类
      Category get(int id);//编辑中得到该分类信息
